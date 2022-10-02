cf4e2a70d8e27f9caf9376c3232d65a79a39b5a7
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-10-02 17:59:24 -0400

    hot fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,74 +27,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Future<void> signIn() async {
-      try {
-        await _googleSignIn.signIn();
-      } catch (e) {
-        print('Error signing in $e');
-      }
-    }
-
-    void signOut() {
-      _googleSignIn.disconnect();
-    }
-
-    Widget _buildWidget() {
-      GoogleSignInAccount? user = _currentUser;
-      if (user != null) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(2, 12, 2, 12),
-          child: Column(
-            children: [
-              ListTile(
-                leading: GoogleUserCircleAvatar(identity: user),
-                title: Text(
-                  user.displayName ?? '',
-                  style: TextStyle(fontSize: 22),
-                ),
-                subtitle: Text(user.email, style: TextStyle(fontSize: 22)),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Signed in successfully',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(onPressed: signOut, child: const Text('Sign out'))
-            ],
-          ),
-        );
-      } else {
-        return Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'You are not signed in',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: signIn,
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('Sign in', style: TextStyle(fontSize: 30)),
-                  )),
-            ],
-          ),
-        );
-      }
-    }
-
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -187,5 +119,73 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  Widget _buildWidget() {
+    GoogleSignInAccount? user = _currentUser;
+    if (user != null) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(2, 12, 2, 12),
+        child: Column(
+          children: [
+            ListTile(
+              leading: GoogleUserCircleAvatar(identity: user),
+              title: Text(
+                user.displayName ?? '',
+                style: TextStyle(fontSize: 22),
+              ),
+              subtitle: Text(user.email, style: TextStyle(fontSize: 22)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Signed in successfully',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: signOut, child: const Text('Sign out'))
+          ],
+        ),
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'You are not signed in',
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: signIn,
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Sign in', style: TextStyle(fontSize: 30)),
+                )),
+          ],
+        ),
+      );
+    }
+  }
+
+  Future<void> signIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (e) {
+      print('Error signing in $e');
+    }
+  }
+
+  void signOut() {
+    _googleSignIn.disconnect();
   }
 }
