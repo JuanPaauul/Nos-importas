785ee6a46b5a678277662ec9658a72993c17eb4d
miky14n <miky14n@gmail.com>
2022-10-26 16:52:57 -0400

    Se agrego el mapa vista general

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nos_importas/functions/input_file.dart';
 import 'package:nos_importas/screens/app_screen.dart';
 
+final _initialCameraPosition = CameraPosition(
+  target: LatLng(-17.3793081, -66.1534358),
+  zoom: 3,
+);
+
 class MapsPage extends StatelessWidget {
   const MapsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,54 +30,7 @@ class MapsPage extends StatelessWidget {
           ),
         ),
       ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    const Text(
-                      "Mapas",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                      ),
-                    ),
-                    const Text(
-                      "Mapa Seguro",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/mapa.png',
-                      width: 450.0,
-                    ),
-                    const Text(
-                      "Mapas Inseguro",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/mapa.png',
-                      width: 450.0,
-                    ),
-                  ],
-                ),
-              ],
-            ))
-          ],
-        ),
-      ),
+      body: GoogleMap(initialCameraPosition: _initialCameraPosition),
     );
   }
 }
