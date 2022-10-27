1913f0a7f4c0bd97b47dda5ad89ccf1ab1b8225d
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-27 10:48:23 -0400

    Hot Fix de camara, dependencias compatibles

--- lib/screens/camera_function.dart
+++ lib/screens/camera_function.dart
@@ -1,145 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 class Imagen extends StatefulWidget {
-  @override
   _ImagenState createState() => _ImagenState();
 }
 
 class _ImagenState extends State<Imagen> {
-  // FUNCION PARA CAMARA
-  //File imagen;
-  final picker = ImagePicker();
-
-  Future selImagen(op) async {
-    var pickedFile;
-
-    if (op == 1) {
-      // ignore: deprecated_member_use
-      pickedFile = await picker.getImage(source: ImageSource.camera);
-    } else {
-      // ignore: deprecated_member_use
-      pickedFile = await picker.getImage(source: ImageSource.gallery);
-    }
-
-    setState(() {
-      if (pickedFile != null) {
-        // FUNCION PARA CAMARA
-        //imagen = File(pickedFile.path);
-      } else {
-        print('No seleccionaste ninguna foto');
-      }
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cámara con Flutter.'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('EMERGENCIA'),
+          onPressed: _optionsDialogBox,
+        ),
+      ),
+    );
   }
 
-  opciones(contex) {
-    showDialog(
+  void _openCamera() {
+    ImagePicker picker = ImagePicker();
+    // ignore: deprecated_member_use
+    final picture = picker.getImage(
+      source: ImageSource.camera,
+    );
+  }
+
+  void _openGallery() {
+    ImagePicker picker = ImagePicker();
+    // ignore: deprecated_member_use
+    final picture = picker.getImage(
+      source: ImageSource.gallery,
+    );
+  }
+
+  Future<void> _optionsDialogBox() {
+    return showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            contentPadding: EdgeInsets.all(0),
             content: SingleChildScrollView(
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      selImagen(1);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom:
-                                  BorderSide(width: 1, color: Colors.grey))),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Tomar una foto',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                          Icon(Icons.camera_alt, color: Colors.blue)
-                        ],
-                      ),
-                    ),
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text('Tomar fotografia'),
+                    onTap: _openCamera,
                   ),
-                  InkWell(
-                    onTap: () {
-                      selImagen(2);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Seleccionar una foto',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                          Icon(Icons.image, color: Colors.blue)
-                        ],
-                      ),
-                    ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
                   ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(color: Colors.red),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Cancelar',
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.white),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  GestureDetector(
+                    child: Text('Seleccionar de galeria'),
+                    onTap: _openGallery,
                   ),
                 ],
               ),
             ),
           );
         });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Selecciona una Imagen'),
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    opciones(context);
-                  },
-                  child: Text('Selecciona una imagen'),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                // FUNCION PARA CAMARA
-                //imagen == null ? Center() : Image.file(imagen)
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
