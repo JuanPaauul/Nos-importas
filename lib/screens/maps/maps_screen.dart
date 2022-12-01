8d6f5324a68987036e7510792d744db4a0cb4c42
miky14n <miky14n@gmail.com>
2022-12-01 13:05:17 -0400

    Last changes

--- lib/screens/maps/maps_screen.dart
+++ lib/screens/maps/maps_screen.dart
@@ -3,64 +3,90 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 /*import 'package:nos_importas/functions/input_file.dart';
 import 'package:nos_importas/screens/app_screen.dart';*/
-
+import 'package:quickalert/quickalert.dart';
 import 'package:nos_importas/screens/maps/maps_controller.dart';
 import 'package:provider/provider.dart';
+
+bool check = false;
+void showAlert(context) {
+  QuickAlert.show(
+    context: context,
+    type: QuickAlertType.error,
+    title: 'Reporte de Zona',
+    text: 'Tu estas fuera del area segura',
+  );
+}
 
 class MapsPage extends StatelessWidget {
   const MapsPage({Key? key}) : super(key: key);
 /*
   @override
   _MapsPageState createState() => _MapsPageState();*/
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<MapsController>(
-        create: (_) {
-          final controller = MapsController();
-          return controller;
-        },
-        child: Scaffold(
-          body: Selector<MapsController, bool>(
-              selector: (_, controller) => controller.loading,
-              builder: (context, loading, child) {
-                return Consumer<MapsController>(builder: (_, controller, __) {
-                  if (!controller.gpsEnabled) {
-                    return Center(
-                      child: Column(mainAxisSize: MainAxisSize.min, children: [
-                        const Text("Tu gps estaapagado"),
-                        ElevatedButton(
-                            onPressed: () {},
-                            child: const Text(
-                              "Prender gps",
-                              textAlign: TextAlign.center,
-                            ))
-                      ]),
-                    );
-                  }
-                  final initialCameraPosition = CameraPosition(
-                    target: LatLng(
-                      controller.initialPosition!.latitude,
-                      controller.initialPosition!.longitude,
-                    ),
-                    zoom: 15,
+      create: (_) {
+        final controller = MapsController();
+        return controller;
+      },
+      child: Scaffold(
+        body: Selector<MapsController, bool>(
+            selector: (_, controller) => controller.loading,
+            builder: (context, loading, child) {
+              return Consumer<MapsController>(builder: (_, controller, __) {
+                if (!controller.gpsEnabled) {
+                  return Center(
+                    child: Column(mainAxisSize: MainAxisSize.min, children: [
+                      const Text("Tu gps estaapagado"),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Prender gps",
+                            textAlign: TextAlign.center,
+                          ))
+                    ]),
                   );
-                  print(controller.initialPosition!.latitude);
-                  print(controller.initialPosition!.longitude);
-                  //controller.newPolygon();
-                  return GoogleMap(
-                    markers: controller.markers,
-                    //polylines: controller.polylines,
-                    polygons: controller.polygons,
-                    //onMapCreated: _controller.onMapCreated,
-                    initialCameraPosition: initialCameraPosition,
-                    myLocationButtonEnabled: true,
-                    myLocationEnabled: true,
-                    //onTap: (position) { print(position); },
-                    onTap: controller.onTap,
+                }
+                final initialCameraPosition = CameraPosition(
+                  target: LatLng(
+                    controller.initialPosition!.latitude,
+                    controller.initialPosition!.longitude,
+                  ),
+                  zoom: 15,
+                );
+                check = controller.checkoutUpdateLocation(LatLng(
+                    controller.initialPosition!.latitude,
+                    controller.initialPosition!.longitude));
+
+                print(check);
+                if (check) {
+                  return QuickAlert.show(
+                    //context: context,
+                    type: QuickAlertType.error,
+                    title: 'Reporte de Zona',
+                    text: 'Tu estas fuera del area segura',
                   );
-                });
-              }),
-        ));
+                }
+                print(controller.initialPosition!.latitude);
+                print(controller.initialPosition!.longitude);
+                //controller.newPolygon();
+
+                return GoogleMap(
+                  markers: controller.markers,
+                  //polylines: controller.polylines,
+                  polygons: controller.polygons,
+                  //onMapCreated: _controller.onMapCreated,
+                  initialCameraPosition: initialCameraPosition,
+                  myLocationButtonEnabled: true,
+                  myLocationEnabled: true,
+                  //onTap: (position) { print(position); },
+                  onTap: controller.onTap,
+                );
+              });
+            }),
+      ),
+    );
   }
 }
 /*
---
+++ lib/screens/maps/services/notification_services.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> initNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('icono_notificacion');
+
+  const DarwinInitializationSettings initializationSettingsIOS =
+      DarwinInitializationSettings();
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+Future<void> showNotificacion() async {
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+    '1',
+    'mapa',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  const NotificationDetails notificationDetails = NotificationDetails(
+    android: androidNotificationDetails,
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+      1,
+      'Reporte de Zona',
+      'Usted se encuentra en una zona',
+      notificationDetails);
+}
