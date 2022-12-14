6c21958c8540d718000b2a945e7d8995732c836e
Sergio55597 <zergio55597@gmail.com>
2022-12-14 18:21:47 -0400

    facha y agrupamiento de vistas

--- lib/screens/send_mail.dart
+++ lib/screens/send_mail.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:nos_importas/screens/utils.dart';
+import 'package:nos_importas/widget/background_email_message.dart';
 
 class SendEmail extends StatefulWidget {
   final String title;
@@ -19,17 +20,22 @@ class _SendEmailState extends State<SendEmail> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
             children: [
-              buildButton(
-                text: 'Enviar Correo',
-                onClicked: () => Utils.openEmail(
-                  // Si son mas de 2 correos separar con ',' dentro la cadena.
-                  toEmail: 'ciudadcochabamba1@gmail.com',
-                  subject: 'ALERTA DE AUXLIO',
-                  body: 'Necesito ayuda, porfavor estoy en apuros !.',
-                ),
+              Background_email_message(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildButton(
+                    text: 'Enviar Correo',
+                    onClicked: () => Utils.openEmail(
+                      // Si son mas de 2 correos separar con ',' dentro la cadena.
+                      toEmail: 'ciudadcochabamba1@gmail.com',
+                      subject: 'ALERTA DE AUXLIO',
+                      body: 'Necesito ayuda, por favor estoy en apuros!',
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
